a06b48bb52d229ad00b4eda0dc70baba83fd7a1f
Shahrukh Khan <srky420@gmail.com>
2026-06-13 23:34:12 +0500

    Update player controls logic, add animations and update killzone

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -320.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +11,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	# Get direction based on actions
+	var direction := Input.get_axis("move_left", "move_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Handle sprite flipping
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# Hanlde animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+
+	# Handle movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
