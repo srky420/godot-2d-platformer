70afd910f93fb05293b51c68b64a5a8f9ec96aca
srky420 <srky420@gmail.com>
2026-06-30 16:06:41 +0500

    Update attack animations and create attack logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,47 +1,35 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coyote_time: Timer = $CoyoteTime
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var _anim: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+@onready var combo_cooldown: Timer = $ComboCooldown
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
-var LEFT_FLOOR = false
+var IS_ATTACKING = false
 
 func _physics_process(delta: float) -> void:
-	
-	if LEFT_FLOOR and is_on_floor():
-		LEFT_FLOOR = false
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		LEFT_FLOOR = true
 
+	if IS_ATTACKING == true:
+		return
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or !coyote_time.is_stopped()):
 		velocity.y = JUMP_VELOCITY
-		LEFT_FLOOR = true
 	
 	# Get direction based on actions
 	var direction := Input.get_axis("move_left", "move_right")
 
 	# Handle sprite flipping
 	if direction > 0:
-		animated_sprite_2d.flip_h = false
+		sprite_2d.flip_h = false
 	elif direction < 0:
-		animated_sprite_2d.flip_h = true
-	
-	# Hanlde animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
-		else:
-			animated_sprite_2d.play("run")
-	else:
-		if velocity.y > 0:
-			animated_sprite_2d.play("fall")
-		else:
-			animated_sprite_2d.play("jump")
+		sprite_2d.flip_h = true
 
 	# Handle movement
 	if direction:
@@ -57,6 +45,25 @@ func _physics_process(delta: float) -> void:
 	# If player falls off, we start coyote timer
 	if was_on_floor and !is_on_floor():
 		coyote_time.start()
+	
 
+func attack() -> bool:
+	if _anim.get_current_node() == "Movement":
+		_anim.travel("Attack1")
+		return true
+	elif _anim.get_current_node() == "Attack1" and !combo_cooldown.is_stopped():
+		_anim.travel("Attack2")
+		return true
+	return false
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack") and is_on_floor():
+		attack()
+		
+
+func switch_attacking(new_val: bool) -> void:
+	IS_ATTACKING = new_val
+	
 	
 	
