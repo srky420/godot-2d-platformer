a9af6c10acd4ad2d0aa5e7244e11f9a4c0575fce
Shahrukh Khan <55321925+srky420@users.noreply.github.com>
2026-07-04 21:16:26 +0500

    Add player state nodes and implement idle state for player

---
+++ scripts/state_machine/player_states/player_idle.gd
@@ -0,0 +1,33 @@
+extends State
+
+# Other state references
+@export var run_state: State
+@export var fall_state: State
+@export var jump_state: State
+
+# Entry
+func enter(prev_state: State) -> void:
+	parent.velocity.x = 0.0
+
+
+func physics_process(delta: float) -> void:
+	# Add the gravity and movement
+	var gravity = parent.get_gravity()
+	if not parent.is_on_floor():
+		parent.velocity += gravity * delta
+	parent.move_and_slide()
+	
+	# Transition to run state
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		state_machine.transition_to(run_state)
+		return
+	
+	# Transition to fall state
+	if not parent.is_on_floor():
+		state_machine.transition_to(fall_state)
+		return
+	
+	# Transition to jump state
+	if Input.is_action_just_pressed("jump"):
+		state_machine.transition_to(jump_state)
