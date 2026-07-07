7f161554eae5c91e94c7361ad68b7ed821dff648
srky420 <srky420@gmail.com>
2026-07-07 15:37:01 +0500

    Add coyote jump to fall state of player

--- scripts/state_machine/player_states/player_fall.gd
+++ scripts/state_machine/player_states/player_fall.gd
@@ -8,6 +8,19 @@ extends State
 # Air movement
 @export var air_speed: float
 
+# Coyote jump data
+@export var coyote_jump_time: float
+var can_coyote_jump: bool = false
+var coyote_timer: float = 0.0
+
+func enter(prev_state: State) -> void:
+	# Check if last state wasn't jump and
+	# Player walked off a ledge
+	coyote_timer = 0.0
+	if prev_state != null and prev_state != jump_state:
+		coyote_timer = coyote_jump_time
+		
+
 func physics_process(delta: float) -> void:
 	# Apply gravity and air movement
 	var gravity = parent.get_gravity()
@@ -21,7 +34,15 @@ func physics_process(delta: float) -> void:
 	if parent.is_on_floor():
 		if direction != 0:
 			state_machine.transition_to(run_state)
+			return
 		else:
 			state_machine.transition_to(idle_state)
+			return
 	
+	# Transition to jump state, coyote time
+	if coyote_timer > 0.0:
+		coyote_timer -= delta
+		if Input.is_action_just_pressed("jump"):
+			state_machine.transition_to(jump_state)
+			return
 	
