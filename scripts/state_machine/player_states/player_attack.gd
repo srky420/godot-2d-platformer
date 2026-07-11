2bdec8e03cdd046d3b6b173d5f3f8596aa97084d
srky420 <srky420@gmail.com>
2026-07-12 02:54:35 +0500

    Add attack state logic and combo logic for player state machine

--- scripts/state_machine/player_states/player_attack.gd
+++ scripts/state_machine/player_states/player_attack.gd
@@ -7,20 +7,27 @@ extends State
 @export var fall_state: State
 
 # Attack and combo cooldowns
-@export var attack_cooldown: float
-@export var combo_cooldown: float
+@export var attack_cooldown: Timer
+@export var combo_cooldown: Timer
 
-# Perform attack on enter
+# Combo counter
+var combo_counter: int = 0
+
+# AnimationTree reference
+var playback: AnimationNodeStateMachinePlayback
+
+# Start cooldowns and play Attack1 anim
 func enter(prev_state: State) -> void:
-	var playback: AnimationNodeStateMachinePlayback = parent.get_node("AnimationTree")["parameters/playback"]
+	playback = parent.get_node("AnimationTree")["parameters/playback"]
 	playback.travel("Attack1")
+	attack_cooldown.start()
+	combo_cooldown.start()
 	
 func physics_process(delta: float) -> void:
-	# Reduce the attack cooldown by delta
-	if attack_cooldown > 0:
-		attack_cooldown -= delta
+	# Don't allow state swich if player is attacking
+	if is_attacking():
 		return
-	
+
 	# Apply gravity
 	var gravity = parent.get_gravity()
 	if not parent.is_on_floor():
@@ -38,8 +45,23 @@ func physics_process(delta: float) -> void:
 			return
 	else:
 		state_machine.transition_to(fall_state)
+		return
 		
-	
 	# Transition to jump
 	if Input.is_action_just_pressed("jump"):
 		state_machine.transition_to(jump_state)
+		return
+
+# Return attacking state based on cooldowns
+func is_attacking() -> bool:
+	if attack_cooldown.is_stopped() and not combo_cooldown.is_stopped():
+		if Input.is_action_just_pressed("attack"):
+			playback.travel("Attack2")
+			attack_cooldown.start()
+			combo_cooldown.start()
+			return true
+	
+	if attack_cooldown.is_stopped() and combo_cooldown.is_stopped():
+		return false
+	
+	return true
